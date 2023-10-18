4dcf40fb2d93e93bbc957cb5557854619cd879a4
ehgud1008 <ehgud1008@naver.com>
2023-10-18 23:42:23 +0900

    DB 연결 및 user스키마 세팅

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI)
-    .then((){ console.log("Connected to DB");})
+    .then(() =>{ console.log("Connected to DB");})
     .catch((error)=> {console.log(error);});
 const app = express()
 const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log('server on! ' + port);
+});
 
 app.get('/', (req, res, next) => {
     res.send('hello world!');
 });
 
-app.listen(port, function () {
-    console.log('server on! ' + port);
-});
+app.use('/api/user', userRouter);
+
