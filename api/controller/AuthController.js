68929d62ae80e4befaec6341503f080ece41998c
ehgud1008 <ehgud1008@naver.com>
2023-10-22 21:31:02 +0900

    회원가입 기능 완료

--- api/controller/AuthController.js
+++ api/controller/AuthController.js
@@ -3,15 +3,14 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 
 export const signUp = async (req, res, next) => {
-    console.log(req.body);
-    const {userName, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({userName, email, password : hashedPassword});
     try {
+        const {userName, email, password} = req.body;
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({userName, email, password : hashedPassword});
         await newUser.save();
-        res.stastus(201).json("유저 생성 성공!");
+        res.status(201).json("유저 생성 성공!");
     } catch (error) {
-        next(errorHandler(550, '유저 생성 실패! 데이터를 확인해주세요.'));
+        next(errorHandler(550, error.message + '///유저 생성 실패! 데이터를 확인해주세요.'));
         // res.tatus(500).json(error.message);
     }
 }
