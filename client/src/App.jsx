c1f673f67024b60dffec5c1e83854ca2055cc16d
ehgud1008 <ehgud1008@naver.com>
2023-10-23 16:30:35 +0900

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
+      <Routes> 
         <Route path='/' element={<Home />} />
         <Route path='/signIn' element={<SignIn />} />
         <Route path='/signUp' element={<SignUp />} />
