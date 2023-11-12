e42c607be1a05d779ef4804cedfd136ecaeab92b
ehgud1008 <ehgud1008@naver.com>
2023-11-12 16:03:15 +0900

    문의하기

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Contact = ( {listing} ) => {
     const [landlord, setLandlord] = useState(null);
+    const [message, setMessage] = useState('');
 
     useEffect( () => {
         const fetchLandlord = async () => {
@@ -15,12 +17,23 @@ const Contact = ( {listing} ) => {
         }
 
         fetchLandlord();
-    }, [listing.userRef])
+    }, [listing.userRef]);
+
+    const changeMessage = (e) => {
+        setMessage(e.target.value)
+   }
+   
   return (
     <>
         {landlord && (
-            <div className=''>
-                <p>연락처 <span>{landlord.username}</span></p>
+            <div className='flex flex-col gap-2'>
+                {/* <p>연락처 <span>{landlord.username}</span></p> */}
+                <textarea name='message' id='message' rows='4' value={message} placeholder='문의하실 내용을 입력해주세요.'
+                        className='w-full border p-3 rounded-lg' onChange={changeMessage} />
+                <Link to={`mailTo:${landlord.email}?subjext=Regarding${listing.name}&body=${message}`}
+                    className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'>
+                    전송
+                </Link>
             </div>
         )}
     </>
