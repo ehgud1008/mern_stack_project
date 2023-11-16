2554c1b808b13f9eca13473205406caa50ec32ff
ehgud1008 <ehgud1008@naver.com>
2023-11-16 12:50:02 +0900

    앱 사전 설치 준비

--- api/index.js
+++ api/index.js
@@ -5,11 +5,15 @@ import userRouter from './routes/user.js';
 import authRouter from './routes/auth.js';
 import listingRouter from './routes/listing.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI, { dbName: 'myfolio' })
     .then(() =>{ console.log("Connected to DB");})
     .catch((error)=> {console.log(error);});
+
+const __dirname = path.resolve();
+
 const app = express()
 app.use(express.json());
 const port = process.env.PORT || 3000;
@@ -27,6 +31,11 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist, index.html'));
+})
 
 app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
